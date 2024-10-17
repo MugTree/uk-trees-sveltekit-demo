1a633f3646f591793398881717c76e3a10431b56
MugTree <waterhose@protonmail.com>
2024-10-17 16:40:50 +0100

    Added a layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="js">
+  import "../pico.css";
+</script>
+
+<svelte:head />
+
+<main class="container">
+  <header>
+    <h3 style="font-size:21px;font-weight:700">
+      <a class="secondary" href="https://mugtree.github.io/uktrees/">UK Trees</a
+      > 🌿
+    </h3>
+  </header>
+  <h1>Gardening site</h1>
+  <hr />
+  <section><slot /></section>
+</main>
+<footer class="container">
+  <hr />
+  <p>© 2024</p>
+</footer>
