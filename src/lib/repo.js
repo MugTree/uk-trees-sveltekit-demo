69cc507a49eef9b4ca4b393ea222c7f640b3cb68
MugTree <waterhose@protonmail.com>
2024-10-17 18:44:40 +0100

    wip

--- src/lib/repo.js
+++ src/lib/repo.js
@@ -1,24 +1,20 @@
 export const getMarkdownData = async () => {
   const markdown = import.meta.glob("../../data/trees/**/*.md");
 
-  console.log("markdown :>> ", markdown);
-
   const iterables = Object.entries(markdown);
 
   let pages = await Promise.all(
     iterables.map(async ([path, page]) => {
       const a = await page();
 
-      console.log("a :>> ", a);
-
-      const prodctSlug = path
+      const treeSlug = path
         .replace(/(\/index)?\.md/, "")
         .split("/")
         .pop();
 
       return {
         meta: a.metadata,
-        slug: prodctSlug,
+        slug: treeSlug,
       };
     })
   );
