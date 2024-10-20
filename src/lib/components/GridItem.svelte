6f177fb6ecda0a09f88af62cd25ed891890bf03d
MugTree <waterhose@protonmail.com>
2024-10-20 16:03:35 +0100

    wip

--- src/lib/components/GridItem.svelte
+++ src/lib/components/GridItem.svelte
@@ -4,18 +4,18 @@
   const thumbnail = (path) => path.replace("trees", "trees/thumbs");
 </script>
 
-<a href={{ base } + "/trees/" + data.slug}
+<a href={"/trees/" + data.slug}
   ><img
     alt=""
     loading="lazy"
     width="340px"
     class="grid-img"
-    src={{ base } + thumbnail(data.meta.image.path)}
+    src={thumbnail(data.meta.image.path)}
   /></a
 >
 
 <h2 class="grid-head">
-  <a href={{ base } + "/trees/" + data.slug}>{data.meta.title}</a>
+  <a href={"/trees/" + data.slug}>{data.meta.title}</a>
 </h2>
 
 <p class="grid-content">{data.meta.summary}</p>
