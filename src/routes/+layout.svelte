b765894891d2d4b69cde3c44260eb58e5ec346a4
MugTree <waterhose@protonmail.com>
2024-11-30 20:29:42 +0000

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,20 +7,11 @@
 
   /** @type {Props} */
   let { children } = $props();
-
-  // console.log(
-  //   process.env.NODE_ENV === "development" ? "environment is development" : ""
-  // );
-  let pro =
-    process.env.BASE_PATH === "uk-trees-sveltekit-demo"
-      ? "uk-trees-sveltekit-demo"
-      : "";
 </script>
 
 <svelte:head />
 
 <main class="container">
-  {pro}
   <header>
     <h3 style="font-size: 21px; font-weight: bold">
       <a class="secondary" href={base}>UK Trees</a> 🌿
